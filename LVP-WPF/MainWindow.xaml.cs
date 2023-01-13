5e0cbb053c9e66a871c5ed30633cc1fd84dc8bb1
Luvai Hassanali <luvaihassanali@gmail.com>
2023-01-13 08:12:45 -0500

    old layout test

--- LVP-WPF/MainWindow.xaml.cs
+++ LVP-WPF/MainWindow.xaml.cs
@@ -18,7 +18,7 @@ namespace LVP_WPF
             // create model
             // set data context -> load gui
             // stop loader
-            this.TvBox.ItemsSource = new MovieData[]
+            this.MovieBox.ItemsSource = new MovieData[]
             {
                 new MovieData{Title="Movie 1", ImageData=LoadImage("image.jpg")},
                 new MovieData{Title="Movie 2", ImageData=LoadImage("image.jpg")},
@@ -28,12 +28,44 @@ namespace LVP_WPF
                 new MovieData{Title="Movie 6", ImageData=LoadImage("image.jpg")},
                 new MovieData{Title="Movie 7", ImageData=LoadImage("image.jpg")}
             };
+            this.TvBox.ItemsSource = new MovieData[]
+            {
+                new MovieData{Title="TV 1", ImageData=LoadImage("image.jpg")},
+                new MovieData{Title="TV 2", ImageData=LoadImage("image.jpg")},
+                new MovieData{Title="TV 3", ImageData=LoadImage("image.jpg")},
+                new MovieData{Title="TV 4", ImageData=LoadImage("image.jpg")},
+                new MovieData{Title="TV 5", ImageData=LoadImage("image.jpg")},
+                new MovieData{Title="TV 6", ImageData=LoadImage("image.jpg")},
+                new MovieData{Title="TV 7", ImageData=LoadImage("image.jpg")},
+                new MovieData{Title="TV 8", ImageData=LoadImage("image.jpg")},
+            };
+            this.CartoonBox.ItemsSource = new MovieData[]
+            {
+                new MovieData{Title="Cartoon 1", ImageData=LoadImage("image.jpg")},
+                new MovieData{Title="Cartoon 2", ImageData=LoadImage("image.jpg")},
+                new MovieData{Title="Cartoon 3", ImageData=LoadImage("image.jpg")},
+                new MovieData{Title="Cartoon 4", ImageData=LoadImage("image.jpg")},
+                new MovieData{Title="Cartoon 5", ImageData=LoadImage("image.jpg")},
+                new MovieData{Title="Cartoon 6", ImageData=LoadImage("image.jpg")},
+                new MovieData{Title="Cartoon 7", ImageData=LoadImage("image.jpg")},
+                new MovieData{Title="Cartoon 8", ImageData=LoadImage("image.jpg")},
+                new MovieData{Title="Cartoon 1", ImageData=LoadImage("image.jpg")},
+                new MovieData{Title="Cartoon 2", ImageData=LoadImage("image.jpg")},
+                new MovieData{Title="Cartoon 3", ImageData=LoadImage("image.jpg")},
+                new MovieData{Title="Cartoon 4", ImageData=LoadImage("image.jpg")},
+                new MovieData{Title="Cartoon 5", ImageData=LoadImage("image.jpg")},
+                new MovieData{Title="Cartoon 6", ImageData=LoadImage("image.jpg")},
+                new MovieData{Title="Cartoon 7", ImageData=LoadImage("image.jpg")},
+                new MovieData{Title="Cartoon 8", ImageData=LoadImage("image.jpg")},
+                new MovieData{Title="Cartoon 7", ImageData=LoadImage("image.jpg")},
+                new MovieData{Title="Cartoon 8", ImageData=LoadImage("image.jpg")}
+            };
         }
 
         int index = 1;
         private BitmapImage LoadImage(string filename)
         {
-            return new BitmapImage(new Uri("C:\\Users\\luv\\Desktop\\temp\\" + (index++).ToString() + ".jpg"));
+            return new BitmapImage(new Uri("C:\\Users\\luv\\Desktop\\lvp-temp\\" + (index++).ToString() + ".jpg"));
         }
     }
 }
