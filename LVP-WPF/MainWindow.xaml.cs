645015ee591899102fc6b5c92afad1f8ed1758be
Luvai Hassanali <luvaihassanali@gmail.com>
2023-01-13 06:50:27 -0500

    add uniform grid

--- LVP-WPF/MainWindow.xaml.cs
+++ LVP-WPF/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using System.Windows.Media.Imaging;
 
 namespace LVP_WPF
 {
@@ -12,10 +14,26 @@ namespace LVP_WPF
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             // start loader
-            Cache.ProcessRootDirectories();
+            //Cache.ProcessRootDirectories();
             // create model
             // set data context -> load gui
             // stop loader
+            this.TvBox.ItemsSource = new MovieData[]
+            {
+                new MovieData{Title="Movie 1", ImageData=LoadImage("image.jpg")},
+                new MovieData{Title="Movie 2", ImageData=LoadImage("image.jpg")},
+                new MovieData{Title="Movie 3", ImageData=LoadImage("image.jpg")},
+                new MovieData{Title="Movie 4", ImageData=LoadImage("image.jpg")},
+                new MovieData{Title="Movie 5", ImageData=LoadImage("image.jpg")},
+                new MovieData{Title="Movie 6", ImageData=LoadImage("image.jpg")},
+                new MovieData{Title="Movie 7", ImageData=LoadImage("image.jpg")}
+            };
+        }
+
+        int index = 1;
+        private BitmapImage LoadImage(string filename)
+        {
+            return new BitmapImage(new Uri("C:\\Users\\luv\\Desktop\\temp\\" + (index++).ToString() + ".jpg"));
         }
     }
 }
