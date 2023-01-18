288e20ad8c3942f01f0db20c1f43794f5ef60f5b
Luvai Hassanali <luvaihassanali@gmail.com>
2023-01-17 20:32:47 -0500

    init media windows

---
+++ LVP-WPF/Windows/SeasonWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LVP_WPF.Windows
+{
+    /// <summary>
+    /// Interaction logic for SeasonWindow.xaml
+    /// </summary>
+    
+    [ObservableObject]
+    public partial class SeasonWindow : Window
+    {
+        public static void Show(Season season)
+        {
+            /*InputDialog dialog = new InputDialog();
+             dialog.Caption = caption;
+             dialog.Message = message;
+             dialog.Topmost = true;
+             dialog.ShowDialog();
+             if ((bool)dialog.DialogResult)
+             {
+                 return true;
+             }
+             return false;*/
+        }
+
+        /*[ObservableProperty]
+        private string caption;
+        [ObservableProperty]
+        private string message;*/
+
+        public SeasonWindow()
+        {
+            DataContext = this;
+            InitializeComponent();
+        }
+
+        private void Button_Season_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
