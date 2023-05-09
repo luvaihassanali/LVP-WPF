c2d8dd0783db1e181e2f5a3f808745d0bb872487
Luvai Hassanali <luvaihassanali@gmail.com>
2023-05-09 09:16:46 -0400

    init remote code

---
+++ LVP-WPF/Utilities/Remote/Remote/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using System.Net.WebSockets;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Remote
+{
+    public partial class Form1 : Form
+    {
+        Samsung remote;
+        public Form1()
+        {
+            InitializeComponent();
+            //WolUtilities.SendTenMagicPackets();
+            remote = new Samsung();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
